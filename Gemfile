d91cfe713ba29bfdad3e2da5c5e190e9a682a82a
Johnneylee Jack Rollins <Johnneylee.Rollins@gmail.com>
2011-06-02 11:33:12 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3'
+
+# Asset template engines
+gem 'sass-rails'
+#gem 'coffee-script'
+gem 'uglifier'
+gem 'therubyracer'
+gem 'jquery-rails'
+
+# Use unicorn as the web server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soapbox::Application.config.secret_token = 'cca8d6cd6d7f1126bff6f3c8495f167623d1d3e3d78ad5b06c0e5912af0f775c2fc60ee79f423fcfe8f8cbf6891943d491204a4970cc550c2021ccfcbfa3e81b'
